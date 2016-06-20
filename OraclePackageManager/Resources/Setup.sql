79a24b879ca49f3d1322a1323f4aefa888722f8f
Stephan Schmidt <schmidt@dltmail.de>
2016-06-20 16:00:58 +0200

    added package manager project

---
+++ OraclePackageManager/Resources/Setup.sql
@@ -0,0 +1,12 @@
+﻿/* Create Schema for Package Manager*/
+BEGIN 
+	EXECUTE IMMEDIATE 'DROP USER oci_pm CASCADE'; 
+EXCEPTION 
+	WHEN OTHERS THEN 
+		dbms_output.put_line(to_char(SQLERRM)); 
+END;
+/
+
+CREATE USER oci_pm IDENTIFIED BY "oci_pm";
+
+
